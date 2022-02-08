3cdf7db45265ac45a33c5c3d7232e0aa36027362
pasindu <adpasindupriyashan@outlook.com>
2022-02-08 13:54:28 +0530

    season check added

--- lib/Controllers/ticketcheck.dart
+++ lib/Controllers/ticketcheck.dart
@@ -4,7 +4,10 @@ import 'package:http/http.dart' as http;
 
 class TicketCheckController {
   Future<Response> checkTicket(qr, station) async {
-    var urlBase = 'http://192.168.1.92:8001/bookings/check/' + qr + '/' + station;
+    var urlBase = 'http://192.168.1.92:8001/bookings' +
+        ((qr.toString().substring(1, 3) != 'S-')
+            ? '/check/' + qr + '/' + station
+            : '/season/check/' + qr);
     var url = Uri.parse(urlBase);
     return await http.get(url);
   }
